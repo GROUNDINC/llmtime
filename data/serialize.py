e98c13a7f30f78a6996f6642082ba9b36ba4da84
liweijia-GROUND <weijia.li@groundinc.co.jp>
2024-06-28 12:12:36 +0900

    Added Azure openai api usage and result consistency checking.

--- data/serialize.py
+++ data/serialize.py
@@ -1,6 +1,8 @@
-from functools import partial
-import numpy as np
 from dataclasses import dataclass
+from functools import partial
+
+import numpy as np
+
 
 def vec_num2repr(val, base, prec, max_val):
     """
@@ -14,7 +16,7 @@ def vec_num2repr(val, base, prec, max_val):
 
     Returns:
     - tuple: Sign and digits in the specified base representation.
-    
+
     Examples:
         With base=10, prec=2:
             0.5   ->    50
@@ -29,24 +31,25 @@ def vec_num2repr(val, base, prec, max_val):
 
     before_decimals = []
     for i in range(max_bit_pos):
-        digit = (val / base**(max_bit_pos - i - 1)).astype(int)
+        digit = (val / base ** (max_bit_pos - i - 1)).astype(int)
         before_decimals.append(digit)
-        val -= digit * base**(max_bit_pos - i - 1)
+        val -= digit * base ** (max_bit_pos - i - 1)
 
     before_decimals = np.stack(before_decimals, axis=-1)
 
     if prec > 0:
         after_decimals = []
         for i in range(prec):
-            digit = (val / base**(-i - 1)).astype(int)
+            digit = (val / base ** (-i - 1)).astype(int)
             after_decimals.append(digit)
-            val -= digit * base**(-i - 1)
+            val -= digit * base ** (-i - 1)
 
         after_decimals = np.stack(after_decimals, axis=-1)
         digits = np.concatenate([before_decimals, after_decimals], axis=-1)
     else:
         digits = before_decimals
     return sign, digits
+
 
 def vec_repr2num(sign, digits, base, prec, half_bin_correction=True):
     """
@@ -66,12 +69,13 @@ def vec_repr2num(sign, digits, base, prec, half_bin_correction=True):
     bs, D = digits.shape
     digits_flipped = np.flip(digits, axis=-1)
     powers = -np.arange(-prec, -prec + D)
-    val = np.sum(digits_flipped/base**powers, axis=-1)
+    val = np.sum(digits_flipped / base**powers, axis=-1)
 
     if half_bin_correction:
-        val += 0.5/base**prec
+        val += 0.5 / base**prec
 
     return sign * val
+
 
 @dataclass
 class SerializerSettings:
@@ -91,18 +95,20 @@ class SerializerSettings:
     - half_bin_correction (bool): If True, applies half bin correction during deserialization. Default is True.
     - decimal_point (str): String representation for the decimal point.
     """
+
     base: int = 10
     prec: int = 3
     signed: bool = True
     fixed_length: bool = False
     max_val: float = 1e7
-    time_sep: str = ' ,'
-    bit_sep: str = ' '
-    plus_sign: str = ''
-    minus_sign: str = ' -'
+    time_sep: str = " ,"
+    bit_sep: str = " "
+    plus_sign: str = ""
+    minus_sign: str = " -"
     half_bin_correction: bool = True
-    decimal_point: str = ''
-    missing_str: str = ' Nan'
+    decimal_point: str = ""
+    missing_str: str = " Nan"
+
 
 def serialize_arr(arr, settings: SerializerSettings):
     """
@@ -116,47 +122,58 @@ def serialize_arr(arr, settings: SerializerSettings):
     - str: String representation of the array.
     """
     # max_val is only for fixing the number of bits in nunm2repr so it can be vmapped
-    assert np.all(np.abs(arr[~np.isnan(arr)]) <= settings.max_val), f"abs(arr) must be <= max_val,\
+    assert np.all(
+        np.abs(arr[~np.isnan(arr)]) <= settings.max_val
+    ), f"abs(arr) must be <= max_val,\
          but abs(arr)={np.abs(arr)}, max_val={settings.max_val}"
-    
+
     if not settings.signed:
         assert np.all(arr[~np.isnan(arr)] >= 0), f"unsigned arr must be >= 0"
-        plus_sign = minus_sign = ''
+        plus_sign = minus_sign = ""
     else:
         plus_sign = settings.plus_sign
         minus_sign = settings.minus_sign
-    
-    vnum2repr = partial(vec_num2repr,base=settings.base,prec=settings.prec,max_val=settings.max_val)
-    sign_arr, digits_arr = vnum2repr(np.where(np.isnan(arr),np.zeros_like(arr),arr))
+
+    vnum2repr = partial(
+        vec_num2repr, base=settings.base, prec=settings.prec, max_val=settings.max_val
+    )
+    sign_arr, digits_arr = vnum2repr(np.where(np.isnan(arr), np.zeros_like(arr), arr))
     ismissing = np.isnan(arr)
-    
+
     def tokenize(arr):
-        return ''.join([settings.bit_sep+str(b) for b in arr])
-    
+        return "".join([settings.bit_sep + str(b) for b in arr])
+
     bit_strs = []
-    for sign, digits,missing in zip(sign_arr, digits_arr, ismissing):
+    for sign, digits, missing in zip(sign_arr, digits_arr, ismissing):
         if not settings.fixed_length:
             # remove leading zeros
             nonzero_indices = np.where(digits != 0)[0]
             if len(nonzero_indices) == 0:
                 digits = np.array([0])
             else:
-                digits = digits[nonzero_indices[0]:]
+                digits = digits[nonzero_indices[0] :]
             # add a decimal point
             prec = settings.prec
             if len(settings.decimal_point):
-                digits = np.concatenate([digits[:-prec], np.array([settings.decimal_point]), digits[-prec:]])
+                digits = np.concatenate(
+                    [digits[:-prec], np.array([settings.decimal_point]), digits[-prec:]]
+                )
         digits = tokenize(digits)
-        sign_sep = plus_sign if sign==1 else minus_sign
+        sign_sep = plus_sign if sign == 1 else minus_sign
         if missing:
             bit_strs.append(settings.missing_str)
         else:
             bit_strs.append(sign_sep + digits)
     bit_str = settings.time_sep.join(bit_strs)
-    bit_str += settings.time_sep # otherwise there is ambiguity in number of digits in the last time step
+    bit_str += (
+        settings.time_sep
+    )  # otherwise there is ambiguity in number of digits in the last time step
     return bit_str
 
-def deserialize_str(bit_str, settings: SerializerSettings, ignore_last=False, steps=None):
+
+def deserialize_str(
+    bit_str, settings: SerializerSettings, ignore_last=False, steps=None
+):
     """
     Deserialize a string into an array of numbers (a time series) based on the provided settings.
 
@@ -167,20 +184,26 @@ def deserialize_str(bit_str, settings: SerializerSettings, ignore_last=False, st
     - steps (int, optional): Number of steps or entries to deserialize.
 
     Returns:
-    - None if deserialization failed for the very first number, otherwise 
+    - None if deserialization failed for the very first number, otherwise
     - np.array: Array of numbers corresponding to the string.
     """
     # ignore_last is for ignoring the last time step in the prediction, which is often a partially generated due to token limit
     orig_bitstring = bit_str
-    bit_strs = bit_str.split(settings.time_sep)
+    # bit_strs = bit_str.split(settings.time_sep)
+    bit_strs = bit_str.split(", ")
     # remove empty strings
     bit_strs = [a for a in bit_strs if len(a) > 0]
     if ignore_last:
         bit_strs = bit_strs[:-1]
     if steps is not None:
         bit_strs = bit_strs[:steps]
-    vrepr2num = partial(vec_repr2num,base=settings.base,prec=settings.prec,half_bin_correction=settings.half_bin_correction)
-    max_bit_pos = int(np.ceil(np.log(settings.max_val)/np.log(settings.base)).item())
+    vrepr2num = partial(
+        vec_repr2num,
+        base=settings.base,
+        prec=settings.prec,
+        half_bin_correction=settings.half_bin_correction,
+    )
+    max_bit_pos = int(np.ceil(np.log(settings.max_val) / np.log(settings.base)).item())
     sign_arr = []
     digits_arr = []
     try:
@@ -190,36 +213,46 @@ def deserialize_str(bit_str, settings: SerializerSettings, ignore_last=False, st
             elif bit_str.startswith(settings.plus_sign):
                 sign = 1
             else:
-                assert settings.signed == False, f"signed bit_str must start with {settings.minus_sign} or {settings.plus_sign}"
-            bit_str = bit_str[len(settings.plus_sign):] if sign==1 else bit_str[len(settings.minus_sign):]
-            if settings.bit_sep=='':
+                assert (
+                    settings.signed == False
+                ), f"signed bit_str must start with {settings.minus_sign} or {settings.plus_sign}"
+            bit_str = (
+                bit_str[len(settings.plus_sign) :]
+                if sign == 1
+                else bit_str[len(settings.minus_sign) :]
+            )
+            if settings.bit_sep == "":
                 bits = [b for b in bit_str.lstrip()]
             else:
                 bits = [b[:1] for b in bit_str.lstrip().split(settings.bit_sep)]
             if settings.fixed_length:
-                assert len(bits) == max_bit_pos+settings.prec, f"fixed length bit_str must have {max_bit_pos+settings.prec} bits, but has {len(bits)}: '{bit_str}'"
+                assert (
+                    len(bits) == max_bit_pos + settings.prec
+                ), f"fixed length bit_str must have {max_bit_pos+settings.prec} bits, but has {len(bits)}: '{bit_str}'"
             digits = []
             for b in bits:
-                if b==settings.decimal_point:
+                if b == settings.decimal_point:
                     continue
                 # check if is a digit
                 if b.isdigit():
                     digits.append(int(b))
                 else:
                     break
-            #digits = [int(b) for b in bits]
+            # digits = [int(b) for b in bits]
             sign_arr.append(sign)
             digits_arr.append(digits)
     except Exception as e:
-        print(f"Error deserializing {settings.time_sep.join(bit_strs[i-2:i+5])}{settings.time_sep}\n\t{e}")
-        print(f'Got {orig_bitstring}')
+        print(
+            f"Error deserializing {settings.time_sep.join(bit_strs[i-2:i+5])}{settings.time_sep}\n\t{e}"
+        )
+        print(f"Got {orig_bitstring}")
         print(f"Bitstr {bit_str}, separator {settings.bit_sep}")
         # At this point, we have already deserialized some of the bit_strs, so we return those below
     if digits_arr:
         # add leading zeros to get to equal lengths
         max_len = max([len(d) for d in digits_arr])
         for i in range(len(digits_arr)):
-            digits_arr[i] = [0]*(max_len-len(digits_arr[i])) + digits_arr[i]
+            digits_arr[i] = [0] * (max_len - len(digits_arr[i])) + digits_arr[i]
         return vrepr2num(np.array(sign_arr), np.array(digits_arr))
     else:
         # errored at first step
--- models/llmtime.py
+++ models/llmtime.py
@@ -1,12 +1,15 @@
-from tqdm import tqdm
-from data.serialize import serialize_arr, deserialize_str, SerializerSettings
 from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass
+
 import numpy as np
 import pandas as pd
-from dataclasses import dataclass
-from models.llms import completion_fns, nll_fns, tokenization_fns, context_lengths
+from tqdm import tqdm
+
+from data.serialize import SerializerSettings, deserialize_str, serialize_arr
+from models.llms import completion_fns, context_lengths, nll_fns, tokenization_fns
 
 STEP_MULTIPLIER = 1.2
+
 
 @dataclass
 class Scaler:
@@ -17,8 +20,10 @@ class Scaler:
         transform (callable): Function to apply transformation.
         inv_transform (callable): Function to apply inverse transformation.
     """
+
     transform: callable = lambda x: x
-    inv_transform: callable = lambda x: x    
+    inv_transform: callable = lambda x: x
+
 
 def get_scaler(history, alpha=0.95, beta=0.3, basic=False):
     """
@@ -37,26 +42,32 @@ def get_scaler(history, alpha=0.95, beta=0.3, basic=False):
     """
     history = history[~np.isnan(history)]
     if basic:
-        q = np.maximum(np.quantile(np.abs(history), alpha),.01)
+        q = np.maximum(np.quantile(np.abs(history), alpha), 0.01)
+
         def transform(x):
             return x / q
+
         def inv_transform(x):
             return x * q
     else:
-        min_ = np.min(history) - beta*(np.max(history)-np.min(history))
-        q = np.quantile(history-min_, alpha)
+        min_ = np.min(history) - beta * (np.max(history) - np.min(history))
+        q = np.quantile(history - min_, alpha)
         if q == 0:
             q = 1
+
         def transform(x):
             return (x - min_) / q
+
         def inv_transform(x):
             return x * q + min_
+
     return Scaler(transform=transform, inv_transform=inv_transform)
+
 
 def truncate_input(input_arr, input_str, settings, model, steps):
     """
     Truncate inputs to the maximum context length for a given model.
-    
+
     Args:
         input (array-like): input time series.
         input_str (str): serialized input time series.
@@ -85,10 +96,13 @@ def truncate_input(input_arr, input_str, settings, model, steps):
                 truncated_input_arr = input_arr[i:]
                 break
         if i > 0:
-            print(f'Warning: Truncated input from {len(input_arr)} to {len(truncated_input_arr)}')
+            print(
+                f"Warning: Truncated input from {len(input_arr)} to {len(truncated_input_arr)}"
+            )
         return truncated_input_arr, truncated_input_str
     else:
         return input_arr, input_str
+
 
 def handle_prediction(pred, expected_length, strict=False):
     """
@@ -107,26 +121,34 @@ def handle_prediction(pred, expected_length, strict=False):
     else:
         if len(pred) < expected_length:
             if strict:
-                print(f'Warning: Prediction too short {len(pred)} < {expected_length}, returning None')
+                print(
+                    f"Warning: Prediction too short {len(pred)} < {expected_length}, returning None"
+                )
                 return None
             else:
-                print(f'Warning: Prediction too short {len(pred)} < {expected_length}, padded with last value')
-                return np.concatenate([pred, np.full(expected_length - len(pred), pred[-1])])
+                print(
+                    f"Warning: Prediction too short {len(pred)} < {expected_length}, padded with last value"
+                )
+                return np.concatenate(
+                    [pred, np.full(expected_length - len(pred), pred[-1])]
+                )
         else:
             return pred[:expected_length]
 
+
 def generate_predictions(
-    completion_fn, 
-    input_strs, 
-    steps, 
-    settings: SerializerSettings, 
+    completion_fn,
+    gpt_client,
+    input_strs,
+    steps,
+    settings: SerializerSettings,
     scalers: None,
-    num_samples=1, 
-    temp=0.7, 
+    num_samples=1,
+    temp=0.7,
     parallel=True,
     strict_handling=False,
     max_concurrent=10,
-    **kwargs
+    **kwargs,
 ):
     """
     Generate and process text completions from a language model for input time series.
@@ -150,25 +172,60 @@ def generate_predictions(
             - completions_list (list of lists): Raw text completions.
             - input_strs (list of str): Serialized input strings.
     """
-    
+
     completions_list = []
-    complete = lambda x: completion_fn(input_str=x, steps=steps*STEP_MULTIPLIER, settings=settings, num_samples=num_samples, temp=temp)
+    complete = lambda x: completion_fn(
+        gpt_client=gpt_client,
+        input_str=x,
+        steps=steps * STEP_MULTIPLIER,
+        settings=settings,
+        num_samples=num_samples,
+        temp=temp,
+    )
     if parallel and len(input_strs) > 1:
-        print('Running completions in parallel for each input')
+        print("Running completions in parallel for each input")
         with ThreadPoolExecutor(min(max_concurrent, len(input_strs))) as p:
-            completions_list = list(tqdm(p.map(complete, input_strs), total=len(input_strs)))
+            completions_list = list(
+                tqdm(p.map(complete, input_strs), total=len(input_strs))
+            )
     else:
         completions_list = [complete(input_str) for input_str in tqdm(input_strs)]
-    def completion_to_pred(completion, inv_transform): 
-        pred = handle_prediction(deserialize_str(completion, settings, ignore_last=False, steps=steps), expected_length=steps, strict=strict_handling)
+
+    def completion_to_pred(completion, inv_transform):
+        pred = handle_prediction(
+            deserialize_str(completion, settings, ignore_last=False, steps=steps),
+            expected_length=steps,
+            strict=strict_handling,
+        )
         if pred is not None:
             return inv_transform(pred)
         else:
             return None
-    preds = [[completion_to_pred(completion, scaler.inv_transform) for completion in completions] for completions, scaler in zip(completions_list, scalers)]
+
+    preds = [
+        [
+            completion_to_pred(completion, scaler.inv_transform)
+            for completion in completions
+        ]
+        for completions, scaler in zip(completions_list, scalers)
+    ]
     return preds, completions_list, input_strs
 
-def get_llmtime_predictions_data(train, test, model, settings, num_samples=10, temp=0.7, alpha=0.95, beta=0.3, basic=False, parallel=True, **kwargs):
+
+def get_llmtime_predictions_data(
+    gpt_client,
+    train,
+    test,
+    model,
+    settings,
+    num_samples=10,
+    temp=0.7,
+    alpha=0.95,
+    beta=0.3,
+    basic=False,
+    parallel=True,
+    **kwargs,
+):
     """
     Obtain forecasts from an LLM based on training series (history) and evaluate likelihood on test series (true future).
     train and test can be either a single time series or a list of time series.
@@ -190,10 +247,12 @@ def get_llmtime_predictions_data(train, test, model, settings, num_samples=10, t
         dict: Dictionary containing predictions, samples, median, NLL/D averaged over each series, and other related information.
     """
 
-    assert model in completion_fns, f'Invalid model {model}, must be one of {list(completion_fns.keys())}'
+    assert (
+        model in completion_fns
+    ), f"Invalid model {model}, must be one of {list(completion_fns.keys())}"
     completion_fn = completion_fns[model]
     nll_fn = nll_fns[model] if model in nll_fns else None
-    
+
     if isinstance(settings, dict):
         settings = SerializerSettings(**settings)
     if not isinstance(train, list):
@@ -204,45 +263,88 @@ def get_llmtime_predictions_data(train, test, model, settings, num_samples=10, t
     for i in range(len(train)):
         if not isinstance(train[i], pd.Series):
             train[i] = pd.Series(train[i], index=pd.RangeIndex(len(train[i])))
-            test[i] = pd.Series(test[i], index=pd.RangeIndex(len(train[i]), len(test[i])+len(train[i])))
+            test[i] = pd.Series(
+                test[i],
+                index=pd.RangeIndex(len(train[i]), len(test[i]) + len(train[i])),
+            )
 
     test_len = len(test[0])
-    assert all(len(t)==test_len for t in test), f'All test series must have same length, got {[len(t) for t in test]}'
+    assert all(
+        len(t) == test_len for t in test
+    ), f"All test series must have same length, got {[len(t) for t in test]}"
 
     # Create a unique scaler for each series
-    scalers = [get_scaler(train[i].values, alpha=alpha, beta=beta, basic=basic) for i in range(len(train))]
+    scalers = [
+        get_scaler(train[i].values, alpha=alpha, beta=beta, basic=basic)
+        for i in range(len(train))
+    ]
 
     # transform input_arrs
     input_arrs = [train[i].values for i in range(len(train))]
-    transformed_input_arrs = np.array([scaler.transform(input_array) for input_array, scaler in zip(input_arrs, scalers)])
+    transformed_input_arrs = np.array(
+        [
+            scaler.transform(input_array)
+            for input_array, scaler in zip(input_arrs, scalers)
+        ]
+    )
     # serialize input_arrs
-    input_strs = [serialize_arr(scaled_input_arr, settings) for scaled_input_arr in transformed_input_arrs]
+    input_strs = [
+        serialize_arr(scaled_input_arr, settings)
+        for scaled_input_arr in transformed_input_arrs
+    ]
     # Truncate input_arrs to fit the maximum context length
-    input_arrs, input_strs = zip(*[truncate_input(input_array, input_str, settings, model, test_len) for input_array, input_str in zip(input_arrs, input_strs)])
-    
+    input_arrs, input_strs = zip(
+        *[
+            truncate_input(input_array, input_str, settings, model, test_len)
+            for input_array, input_str in zip(input_arrs, input_strs)
+        ]
+    )
+
     steps = test_len
     samples = None
     medians = None
     completions_list = None
     if num_samples > 0:
-        preds, completions_list, input_strs = generate_predictions(completion_fn, input_strs, steps, settings, scalers,
-                                                                    num_samples=num_samples, temp=temp, 
-                                                                    parallel=parallel, **kwargs)
-        samples = [pd.DataFrame(preds[i], columns=test[i].index) for i in range(len(preds))]
+        preds, completions_list, input_strs = generate_predictions(
+            completion_fn,
+            gpt_client,
+            input_strs,
+            steps,
+            settings,
+            scalers,
+            num_samples=num_samples,
+            temp=temp,
+            parallel=parallel,
+            **kwargs,
+        )
+        samples = [
+            pd.DataFrame(preds[i], columns=test[i].index) for i in range(len(preds))
+        ]
         medians = [sample.median(axis=0) for sample in samples]
         samples = samples if len(samples) > 1 else samples[0]
         medians = medians if len(medians) > 1 else medians[0]
     out_dict = {
-        'samples': samples,
-        'median':  medians,
-        'info': {
-            'Method': model,
+        "samples": samples,
+        "median": medians,
+        "info": {
+            "Method": model,
         },
-        'completions_list': completions_list,
-        'input_strs': input_strs,
+        "completions_list": completions_list,
+        "input_strs": input_strs,
     }
     # Compute NLL/D on the true test series conditioned on the (truncated) input series
     if nll_fn is not None:
-        BPDs = [nll_fn(input_arr=input_arrs[i], target_arr=test[i].values, settings=settings, transform=scalers[i].transform, count_seps=True, temp=temp) for i in range(len(train))]
-        out_dict['NLL/D'] = np.mean(BPDs)
+        BPDs = [
+            nll_fn(
+                gpt_client,
+                input_arr=input_arrs[i],
+                target_arr=test[i].values,
+                settings=settings,
+                transform=scalers[i].transform,
+                count_seps=True,
+                temp=temp,
+            )
+            for i in range(len(train))
+        ]
+        out_dict["NLL/D"] = np.mean(BPDs)
     return out_dict
